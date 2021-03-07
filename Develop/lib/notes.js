f7348894efcd3b2cd83589dbe17b45554581f58a
Drew Vena <drewvena1@gmail.com>
2021-03-07 14:37:11 -0600

    add delete and save funcionality to notes, debugged starter code errors

---
+++ Develop/lib/notes.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+let notes = require('../db/db.json');
+const { nanoid } = require('nanoid');
+
+function validateNote(note) {
+    if (note.title === '' || !note.title) {
+        return false;
+    } 
+    if (note.text === '' || !note.text) {
+        return false;
+    }
+    return true;
+}
+
+function writeToFile(note) {
+    const newNote = note;
+    newNote.id = nanoid();
+    notes.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(notes, null, 2)
+    );
+}
+
+function deleteFile(note) {
+    const result = notes.filter(exNote => {
+        if (exNote !== note) {
+            return exNote;
+        }
+    });
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(result, null, 2)
+    );
+    notes = result;
+    return result;
+}
+
+function findNote(id) {
+    const result = notes.filter(note => {
+        if (note.id === id) {
+            return note;
+        }
+    });
+    return result;
+}
+
+module.exports = {
+    validateNote,
+    writeToFile,
+    deleteFile,
+    findNote
+}
+
--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
+
 const { validateNote, writeToFile, deleteFile, findNote } = require('./Develop/lib/notes');
 app.use(express.static('Develop/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+let notes = require("./Develop/db/db.json");
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
